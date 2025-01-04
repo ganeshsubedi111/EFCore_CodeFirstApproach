6625c49ec43c5cf9cc6d2591b431351d90c4e91d
ganeshsubedi111 <subediganesh306@gmail.com>
2025-01-04 13:48:44 +0545

    Added gitignore

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using EF_CodeFirstApproach.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace EF_CodeFirstApproach.Controllers
@@ -19,8 +20,84 @@ namespace EF_CodeFirstApproach.Controllers
             return View(stdData);
         }
 
-     
+        public IActionResult Create()
+        {
+          
+            return View();
+        }
 
-        
+        [HttpPost]
+      
+        public async Task<IActionResult> Create(Student std)
+        {
+            
+
+            if (ModelState.IsValid)
+            {
+
+                await studentDbContext.Students.AddAsync(std);
+                await studentDbContext.SaveChangesAsync();
+                TempData["Success"] = "Record Inserted";
+
+                return RedirectToAction("Index","Home");
+            }
+
+           
+            return View(std);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            var student = await studentDbContext.Students.FindAsync(id);
+            if (student == null)
+            {
+                return NotFound();  
+            }
+            return View(student);  
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int? id ,Student std)
+        {
+            if (id != std.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+
+                 studentDbContext.Students.Update(std);
+                await studentDbContext.SaveChangesAsync();
+                TempData["Success"] = "Record Updated";
+
+                return RedirectToAction("Index", "Home");
+            }
+           
+            return View(std);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var student = await studentDbContext.Students.FindAsync(id);
+
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+   
+            studentDbContext.Students.Remove(student);
+            await studentDbContext.SaveChangesAsync();
+            TempData["Success"] = "Record Deleted";
+            return RedirectToAction("Index", "Home");
+
+        }
+
     }
 }
